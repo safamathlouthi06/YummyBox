c81dfd72689ecdac962e4248e4f232113d7a3263
SafaMathlouthi <safa.mathlouthi20@gmail.com>
2025-12-10 21:58:09 +0100

    amelioration css

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='accounts/logout.html'), name='logout'),
+    path('logout/', auth_views.LoginView.as_view(template_name='accounts/logout.html'), name='logout'),
+
+   
 ]
